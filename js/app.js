4dd4fde57403210683bd5da68ea4d138bf4dc7b3
Ahmedmeid99 <ahmedalbakrycool@gmail.com>
2025-09-17 15:20:11 +0300

    add pages

--- js/app.js
+++ js/app.js
@@ -8,12 +8,15 @@ const TEMPLATE = "YOUR_TEMPLATE_ID"; // Replace with your EmailJS Template ID
     emailjs.init("YOUR_PUBLIC_KEY"); // Replace with your EmailJS Public Key
 })();
 
-document.getElementById("contact-form").addEventListener("submit", function (e) {
-    e.preventDefault();
-    emailjs.sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", this)
-        .then(() => alert("Message sent successfully!"))
-        .catch(err => alert("Error: " + JSON.stringify(err)));
-});
+const contactForm = document.getElementById("contact-form")
+if (contactForm) {
+    contactForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+        emailjs.sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", this)
+            .then(() => alert("Message sent successfully!"))
+            .catch(err => alert("Error: " + JSON.stringify(err)));
+    });
+}
 
 //------------------------------------------------
 const scrollBtn = document.querySelector('.scroll-btn')
@@ -52,3 +55,37 @@ navLinks.forEach(link => {
         link.classList.add("active");
     });
 });
+
+// -------------------------------------------
+const statusFilter = document.getElementById("statusFilter");
+const sectorFilter = document.getElementById("sectorFilter");
+const regionFilter = document.getElementById("regionFilter");
+const projectCards = document.querySelectorAll(".project-card");
+
+function filterProjects() {
+    const statusValue = statusFilter.value;
+    const sectorValue = sectorFilter.value;
+    const regionValue = regionFilter.value;
+
+    projectCards.forEach(card => {
+        const status = card.dataset.status;
+        const sector = card.dataset.sector;
+        const region = card.dataset.region;
+
+        // تحقق إذا المشروع ينطبق عليه الفلاتر
+        if (
+            (statusValue === "all" || status === statusValue) &&
+            (sectorValue === "all" || sector === sectorValue) &&
+            (regionValue === "all" || region === regionValue)
+        ) {
+            card.style.display = "block";
+        } else {
+            card.style.display = "none";
+        }
+    });
+}
+
+// Event Listeners
+statusFilter.addEventListener("change", filterProjects);
+sectorFilter.addEventListener("change", filterProjects);
+regionFilter.addEventListener("change", filterProjects);
