3b4edcc48f8457c7862516c407d911f435771642
Ahmedmeid99 <ahmedalbakrycool@gmail.com>
2025-09-18 19:25:43 +0300

    add pages

--- js/app.js
+++ js/app.js
@@ -45,15 +45,6 @@ scrollBtn.addEventListener('click', () => {
     })
 })
 
-// --------------------------
-// const navLinks = document.querySelectorAll(".nav-link");
-
-// navLinks.forEach(link => {
-//     link.addEventListener("click", () => {
-//         navLinks.forEach(l => l.classList.remove("active"));
-//         link.classList.add("active");
-//     });
-// });
 
 // -------------------------------------------
 const statusFilter = document.getElementById("statusFilter");
