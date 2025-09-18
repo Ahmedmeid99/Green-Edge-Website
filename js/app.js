356651fafe3fb1ad09518bd4dba79fc8a9faf308
Ahmedmeid99 <ahmedalbakrycool@gmail.com>
2025-09-18 19:08:10 +0300

    add pages

--- js/app.js
+++ js/app.js
@@ -1,24 +1,30 @@
-const PUBLIC_KEY = "YOUR_PUBLIC_KEY"; // Replace with your EmailJS Public Key
-const SERVICE = "YOUR_SERVICE_ID"; // Replace with your EmailJS Service ID
-const TEMPLATE = "YOUR_TEMPLATE_ID"; // Replace with your EmailJS Template ID
+const PUBLIC_KEY = "ThijL73HGDMaw4i7L"; // EmailJS Public Key
+const SERVICE = "service_hyf2ufd"; // EmailJS Service ID
+const TEMPLATE = "template_1usngjq"; // EmailJS Template ID
 
 // Initialize EmailJS
 
+// auto call function 
 (function () {
-    emailjs.init("YOUR_PUBLIC_KEY"); // Replace with your EmailJS Public Key
+    emailjs.init(PUBLIC_KEY);
 })();
 
 const contactForm = document.getElementById("contact-form")
 if (contactForm) {
     contactForm.addEventListener("submit", function (e) {
         e.preventDefault();
-        emailjs.sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", this)
-            .then(() => alert("Message sent successfully!"))
-            .catch(err => alert("Error: " + JSON.stringify(err)));
+        emailjs.sendForm(SERVICE, TEMPLATE, this)
+            .then(() => {
+                alert("Message sent successfully!")
+                // clear form inputs
+                contactForm.reset();
+            }
+            ).catch(err => alert("Error: " + JSON.stringify(err)));
     });
 }
 
 //------------------------------------------------
+// show scrollBtn 
 const scrollBtn = document.querySelector('.scroll-btn')
 // OnScroll event
 window.addEventListener('scroll', () => {
@@ -38,23 +44,16 @@ scrollBtn.addEventListener('click', () => {
         behavior: 'smooth'
     })
 })
-//Scroll to top by scroll-btn
-scrollBtn.addEventListener('click', () => {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    })
-})
 
 // --------------------------
-const navLinks = document.querySelectorAll(".nav-link");
+// const navLinks = document.querySelectorAll(".nav-link");
 
-navLinks.forEach(link => {
-    link.addEventListener("click", () => {
-        navLinks.forEach(l => l.classList.remove("active"));
-        link.classList.add("active");
-    });
-});
+// navLinks.forEach(link => {
+//     link.addEventListener("click", () => {
+//         navLinks.forEach(l => l.classList.remove("active"));
+//         link.classList.add("active");
+//     });
+// });
 
 // -------------------------------------------
 const statusFilter = document.getElementById("statusFilter");
